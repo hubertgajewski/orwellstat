43ed5341499133df952b21c72030bd3ba6d836c9
Hubert Gajewski <7265871+hubertgajewski@users.noreply.github.com>
2026-04-13 18:14:04 +0200

    test: break selector for self-healing smoke test

--- playwright/typescript/tests/navigation.spec.ts
+++ playwright/typescript/tests/navigation.spec.ts
@@ -31,7 +31,7 @@ test.describe('navigation', { tag: '@smoke' }, () => {
   test('home page', async ({ page }) => {
     await page
       .locator('#menubar')
-      .getByRole('link', { name: 'Strona główna', exact: true })
+      .getByRole('link', { name: 'Strona glowna', exact: true })
       .click();
 
     await expectHeadings(page, [
